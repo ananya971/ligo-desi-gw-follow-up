51fa0ba3912fbb4bfb021a7f6ba4f568b9b8270b
Ananya Shankar <ashankar@login16.chn>
2023-07-02 06:54:43 -0700

    redmine commands update

---
+++ Shell Commands/S230627c_redmine_commands/S230627c_redmine_commands.sh
@@ -0,0 +1,163 @@
+
+2mt 90
+2mf bg ri nJ
+2mobj ligodesi_S230627c_103833_465807 3441
+2mradec 10 38 36.91   46 58 7.31
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=159.6538&dec=46.9687&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=159.6538&dec=46.9687&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104722_503304 3441
+2mradec 10 47 26.64   50 33 4.36
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=161.861&dec=50.5512&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=161.861&dec=50.5512&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_103244_391322 3441
+2mradec 10 32 47.22   39 13 22.74
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=158.1967&dec=39.223&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=158.1967&dec=39.223&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105537_614656 3441
+2mradec 10 55 42.59   61 46 56.66
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=163.9275&dec=61.7824&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=163.9275&dec=61.7824&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104809_565459 3441
+2mradec 10 48 13.98   56 54 59.46
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=162.0582&dec=56.9165&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=162.0582&dec=56.9165&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_102844_371353 3441
+2mradec 10 28 47.80   37 13 53.93
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=157.1992&dec=37.2316&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=157.1992&dec=37.2316&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105007_552442 3441
+2mradec 10 50 11.83   55 24 42.80
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=162.5493&dec=55.4119&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=162.5493&dec=55.4119&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104925_575421 3441
+2mradec 10 49 30.28   57 54 21.56
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=162.3762&dec=57.906&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=162.3762&dec=57.906&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104002_493304 3441
+2mradec 10 40 6.30   49 33 4.58
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=160.0263&dec=49.5513&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=160.0263&dec=49.5513&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105125_573544 3441
+2mradec 10 51 30.04   57 35 44.09
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=162.8752&dec=57.5956&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=162.8752&dec=57.5956&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_102843_370530 3441
+2mradec 10 28 46.83   37 5 30.54
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=157.1951&dec=37.0918&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=157.1951&dec=37.0918&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_103438_393828 3441
+2mradec 10 34 41.63   39 38 28.16
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=158.6735&dec=39.6412&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=158.6735&dec=39.6412&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105219_540352 3441
+2mradec 10 52 23.32   54 3 52.50
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=163.0972&dec=54.0646&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=163.0972&dec=54.0646&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104650_575016 3441
+2mradec 10 46 55.31   57 50 16.53
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=161.7305&dec=57.8379&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=161.7305&dec=57.8379&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_103328_360559 3441
+2mradec 10 33 31.45   36 5 59.55
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=158.381&dec=36.0999&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=158.381&dec=36.0999&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_102946_401913 3441
+2mradec 10 29 49.85   40 19 13.58
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=157.4577&dec=40.3204&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=157.4577&dec=40.3204&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104734_591240 3441
+2mradec 10 47 39.55   59 12 40.73
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=161.9148&dec=59.2113&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=161.9148&dec=59.2113&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105631_572051 3441
+2mradec 10 56 36.04   57 20 51.90
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=164.1502&dec=57.3477&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=164.1502&dec=57.3477&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_104330_512128 3441
+2mradec 10 43 33.80   51 21 28.94
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=160.8908&dec=51.358&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=160.8908&dec=51.358&layer=ls-dr10&pixscale=0.262&bands=griz
+2mobj ligodesi_S230627c_105716_664510 3441
+2mradec 10 57 21.94   66 45 10.79
+2mdithersize 10
+2mh best
+2mautofocus
+2mo 5
+#open https://www.legacysurvey.org/viewer/jpeg-cutout?ra=164.3414&dec=66.753&layer=ls-dr10&pixscale=0.262
+#wget https://www.legacysurvey.org/viewer/fits-cutout?ra=164.3414&dec=66.753&layer=ls-dr10&pixscale=0.262&bands=griz
